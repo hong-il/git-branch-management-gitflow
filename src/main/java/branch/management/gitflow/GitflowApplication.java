1c986acb9da88ece15046c8952144ed01ea60abc
KIM HONGIL <clroyan01@gmail.com>
2020-01-01 21:42:41 +0900

    Create git flow project

---
+++ src/main/java/branch/management/gitflow/GitflowApplication.java
@@ -0,0 +1,13 @@
+package branch.management.gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/branch/management/gitflow/GitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package branch.management.gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
